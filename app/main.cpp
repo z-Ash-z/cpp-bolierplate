de2deb8881e7b815f8f2af70dcde6252ce9ce156
Aneesh Chodisetty <ch.aneesh1996@gmail.com>
2022-10-12 22:07:44 -0400

    Fixed all bugs and updated the screenshot

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include <AnalogSensor.hpp>
 
 int main()
@@ -11,23 +10,5 @@ int main()
     {
         std::cout << "DONE" << std::endl;
     }
-=======
-#include <lib.hpp>
-#include <vector>
-
-int main()
-{
-    dummy();
-
-    bool condition{true};
-    if (condition)
-    {
-        std::cout << "Hakuna matata" << std::endl;
-    }
-
-    std::vector<int> test { 1, 2, 3, 4, 5 };
-    test[4] = 0;
-
->>>>>>> c73d6b794f1bd2b731ea38f46a23ec2e57fa8c1f
     return 0;
 }
